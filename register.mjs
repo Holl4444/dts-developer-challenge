3911dd3fc8bb7ad62e76fc2ef5219e28fc2d38b6
Holl4444 <holly.cello@gmail.com>
2025-04-09 22:14:48 +0100

    update: modern tsx approach

--- register.mjs
+++
@@ -1,5 +0,0 @@
-// register.mjs. Allow ts files to be used directly by Node.js in ESModules
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-register('ts-node/esm', pathToFileURL('./'));
